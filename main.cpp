5bf7902efdeb19b1b70aab20aab30ed42c44b8b8
Francisco José Fajardo Toril <frannnlol@gmail.com>
2016-01-28 11:28:20 +0100

    Funciones E/S básicas.

--- main.cpp
+++ main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
 #include <opencv2\opencv.hpp>
 using namespace std;
+using namespace cv;
+
+////// Funciones auxiliares
+//// Entrada / Salida
+// Lee la imagen 'filename' en color o escala de grises en función del parámetro 'flagColor'.
+Mat leeimagen(char* filename, int flagColor){
+	return imread(filename, flagColor);
+}
+
+// Muesta una imagen por pantalla en una ventana de nombre 'nombre_ventana'.
+void pintaI(const Mat& im, char* nombre_ventana = "imagen"){
+	Mat im_copia = Mat(im);
+	im_copia.convertTo(im_copia, CV_8U);
+
+	namedWindow(nombre_ventana, 1);
+	imshow(nombre_ventana, im_copia);
+	waitKey();
+	destroyWindow(nombre_ventana);
+}
+
 int main(){
 
 	
